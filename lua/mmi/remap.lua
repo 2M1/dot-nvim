41e779ccb65f691f0754dfdf8dfecfd7b76fc5d2
Markus M <mmi.github@gmail.com>
2024-06-14 14:11:27 +0200

    feat(mapping): remaped delte (d) to not rewrite the standard register by default.

--- lua/mmi/remap.lua
+++ lua/mmi/remap.lua
@@ -5,8 +5,19 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- keep cursor in the middle when jumping:
+-- better delete functions:
+vim.keymap.set("n", "d", "\"_d", {noremap=true})
+vim.keymap.set("v", "d", "\"_d", {noremap=true})
+vim.keymap.set("n", "<leader>d", "d", {noremap=true})
+vim.keymap.set("v", "<leader>d", "d", {noremap=true})
 
+vim.keymap.set("n", "D", "\"_D", {noremap=true})
+vim.keymap.set("v", "D", "\"_D", {noremap=true})
+
+vim.keymap.set("n", "<leader>D", "D", {noremap=true})
+vim.keymap.set("v", "<leader>D", "D", {noremap=true})
+
+--- keep cursor in the middle when jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
